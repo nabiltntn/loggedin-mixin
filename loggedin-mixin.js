2a51b0c93c369013421700cb522e620590b103b8
nabiltntn <zouabi.nabil@live.com>
2016-01-26 16:14:57 +0000

    fix Error constructor params

--- loggedin-mixin.js
+++ loggedin-mixin.js
@@ -1,6 +1,5 @@
 /* global LoggedInMixin:true */
 LoggedInMixin = function(methodOptions) {
-    console.log(JSON.stringify(methodOptions));
     check(methodOptions.checkLoggedInError, Match.ObjectIncluding({
         error: String,
         message: String,
@@ -9,9 +8,8 @@ LoggedInMixin = function(methodOptions) {
     const runFunc = methodOptions.run;
     methodOptions.run = function() {
         if (!this.userId) {
-            throw new Meteor.Error(...methodOptions.checkLoggedInError);
+            throw new Meteor.Error(...Object.values(methodOptions.checkLoggedInError));
         };
-        console.log(JSON.stringify(arguments));
         return runFunc(...arguments);
     }
     return methodOptions;
